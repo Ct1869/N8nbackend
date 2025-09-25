0e5c977d4eb2e30356e1351c28aae9ed44794eff
Ct1869 <167578329+Ct1869@users.noreply.github.com>
2025-09-25 15:14:26 +0200

    server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(cors());
 
 // MongoDB connection
 mongoose.connect(
-  "mongodb+srv://oliphantcurtis4_db_user:dr9ygqBYbZGdJ40Z@n8n.rzaymxp.mongodb.net/?retryWrites=true&w=majority&appName=n8n"
+  "mongodb+srv://jmc_db_user:qLfR7JDrc3I2cFye@cluster0.phone-manager.mongodb.net/?retryWrites=true&w=majority"
 );
 
 // Schema & Model
